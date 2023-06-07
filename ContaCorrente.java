23ac25327f4d22281242b50920190b423ae20869
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2023-06-07 20:47:33 -0300

    adicionando os atributos e métodos a serem implementados

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -2,5 +2,17 @@
 
 public class ContaCorrente {
 
-
+  // TODO
+  // Número da conta
+  // Número da agência
+  // Nome do cliente
+  // Data de nascimento
+  // Saldo da conta
+  
+  // Sacar um valor
+  // Transferir um valor para outra conta 
+  // Cancelar a conta com uma justificativa
+  // Consultar o extrato entre duas datas
+  // Consultar o saldo atual
+  
 }
