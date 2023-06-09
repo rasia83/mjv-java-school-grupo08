e14305dd38720804ef8cc29c5c671d9600cee9c6
Tiago Edgar Rasia <tiago_rasia@hotmail.com>
2023-06-09 14:33:14 -0300

    Criando um teste para ContaCorrente

---
+++ ContaCorrenteTeste.java
@@ -0,0 +1,13 @@
+
+
+public class ContaCorrenteTeste {
+
+	public static void main(String[] args) {
+		
+		ContaCorrente c1 = new ContaCorrente();
+
+		System.out.println("Fim dos testes");
+	}
+
+
+}
