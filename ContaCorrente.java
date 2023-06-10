4a8c0628ca95ec14eff55764fe4ed57218901b04
victoralves <jvicme@gmail.com>
2023-06-10 19:12:29 -0300

    Adicionado atributo numeroAgencia e o metodo Consultar extraato

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -37,6 +37,18 @@ public class ContaCorrente {
             System.out.println("Transferencia para a conta:" + numeroContaDestino + " realizada com sucesso");
             System.out.println("Saldo atual:" + saldoConta);
         }
+    public List<Transacao> consultarExtrato(Date dataInicial, Date dataFinal) {
+    	List<Transacao> extrato = new ArrayList<>();
+
+    	for (Transacao transacao : historicoTransacoes) {
+        	if (transacao.getData().after(dataInicial) && transacao.getData().before(dataFinal)) {
+            	    extrato.add(transacao);
+        } 
+	//Isso seria apenas um rascunho, já que seria necessário criar algo como a classe Transacao e outros requisitos para que isso fosse excutado corretamente
+    }
+
+    return extrato;
+}
     }
 
 }
