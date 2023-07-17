694705daade8acd3e04a09b7ebe30212687d3c07
Tiago Caixeta Deoldoto <50558787+deoldoto@users.noreply.github.com>
2023-07-17 14:24:39 -0300

    Adição swagger API, serviço de profissões

---
+++ Jobby/src/main/java/com/mjvinnovation/school/jobby/webservice/ProfissaoRessource.java
@@ -0,0 +1,34 @@
+package com.mjvinnovation.school.jobby.webservice;
+
+import com.mjvinnovation.school.jobby.model.Profissao;
+import com.mjvinnovation.school.jobby.service.ProfissaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/profissoes")
+public class ProfissaoRessource {
+
+    @Autowired
+    private ProfissaoService service;
+
+
+    @GetMapping("/All")
+    public List<Profissao> getAll(){
+        return service.listarTodas();
+    }
+
+    @PostMapping
+    public void post(@RequestBody Profissao request){
+
+        service.salvar(request);
+
+
+    }
+
+
+
+
+}
